96e859e74f30d3a08bc5472198ef109b45b805ea
ehsan9 <h.ehsan24@gmail.com>
2022-11-20 23:31:40 +0330

    feat(tests): Improved parser function for nested json and implement testParseJsonNestedFormationAndStoreInDatabase for true assertion this part

--- app/Infrastructure/ResourceData.php
+++ app/Infrastructure/ResourceData.php
@@ -6,32 +6,91 @@ namespace App\Infrastructure;
 
 use App\Infrastructure\Abstracts\ObjectResponseAbstract;
 use App\Infrastructure\Interfaces\DataInterface;
+use Illuminate\Support\Facades\Log;
 
 class ResourceData extends ObjectResponseAbstract implements DataInterface
 {
-    public function parseData($formatKeys = ['items' => 'data']): array
+    public function parseData($formatKeys = ['data' => ['key' => 'item', 'type' => 'JSON']]): array
     {
         $object = $this->toObject();
         $this->data = null;
 
         if ($object && $this->hasStandardFormat($formatKeys)) {
-            foreach ($formatKeys as $key => $value) {
-                $this->data[$key] = $object->{$value};
+            $mappedKeys = $this->getMappedKeys($formatKeys, $object);
+
+            $duplicatedIndexValues = [];
+            foreach ($mappedKeys as $key => $indexes) {
+                if (is_array($object->{$key})) {
+                    $i = 0;
+                    foreach ($object->{$key} as $item) {
+                        foreach ($indexes as $keyIndex => $valIndex) {
+                            $this->data[$i][$keyIndex] = isset($item->{$valIndex}) ?
+                                $this->convert($item->{$valIndex}, "$key.$valIndex", $formatKeys) : null;
+                        }
+                        foreach ($duplicatedIndexValues as $duplicatedIndexValue) {
+                            $this->data[$i][$duplicatedIndexValue['key']] = $duplicatedIndexValue['value'];
+                        }
+                        $i++;
+                    }
+                } else {
+                    $duplicatedIndexValues[] = [
+                        'key' => array_key_first($indexes),
+                        'value' => isset($object->{$key}) ? $object->{$key} : null,
+                    ];
+                    $this->data[][array_key_first($indexes)] = isset($object->{$key}) ? $object->{$key} : null;
+                }
             }
         }
 
         return $this->data;
     }
 
-    public function hasStandardFormat($formatKeys = ['items' => 'data']): bool
+    public function hasStandardFormat($formatKeys = ['data' => ['key' => 'item', 'type' => 'JSON']]): bool
     {
         $object = $this->toObject();
         foreach ($formatKeys as $key => $value) {
-            if (!property_exists($object, $value)) {
-                return false;
+            $nestedArray = explode('.', $key);
+            if (count($nestedArray) > 1) {
+                if (!is_array($object->{$nestedArray[0]}) &&
+                    !property_exists($object->{$nestedArray[0]}[0], $nestedArray[1])) {
+                    return false;
+                }
+            } else {
+                if (!property_exists($object, $key)) {
+                    Log::error('format for parsing not valid: ', [$key, $object]);
+                    return false;
+                }
             }
         }
 
         return true;
+    }
+
+    private function convert($value, string $keyIndex, $formatKeys): mixed
+    {
+        if($formatKeys[$keyIndex]['type'] == 'JSON') {
+            return json_encode($value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param array $formatKeys
+     * @param object $object
+     * @return array
+     */
+    private function getMappedKeys(array $formatKeys, object $object): array
+    {
+        $mappedKeys = [];
+        foreach ($formatKeys as $key => $value) {
+            $nestedArray = explode('.', $key);
+            if (count($nestedArray) > 1 && is_array($object->{$nestedArray[0]})) {
+                $mappedKeys[$nestedArray[0]][$value['key']] = $nestedArray[1];
+            } else {
+                $mappedKeys[$nestedArray[0]] = [$value['key'] => $key];
+            }
+        }
+        return $mappedKeys;
     }
 }
