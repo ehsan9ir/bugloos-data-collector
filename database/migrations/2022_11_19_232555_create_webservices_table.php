781312459e7094f4f8d180a11c671008a435d16d
ehsan9 <h.ehsan24@gmail.com>
2022-11-21 00:05:22 +0330

    feat(migration): create webservices, webservice_requests and general_data_responses basically tables

---
+++ database/migrations/2022_11_19_232555_create_webservices_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\Webservice;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Symfony\Component\HttpFoundation\Request;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('webservices', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 120);
+            $table->string('url', 240)->unique();
+            $table->enum('request_method', [Request::METHOD_GET, Request::METHOD_POST])->default(Request::METHOD_GET);
+            $table->json('payload')->nullable();
+            $table->string('token', 240)->nullable();
+            $table->json('response_template');
+            $table->enum('response_type', Webservice::$responseTypes)->default(Webservice::JSON_TYPE_RESPONSE);
+            $table->enum('storage_type', Webservice::$storageTypes)->default(Webservice::GENERAL_TYPE_STORAGE);
+            $table->string('schedule_frequency', 80)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('webservices');
+    }
+};
